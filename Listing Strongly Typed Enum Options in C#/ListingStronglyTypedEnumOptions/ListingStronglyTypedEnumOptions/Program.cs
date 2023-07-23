61e418ed8e7d3785b6cbd33857d62b4a5b607056
Aly Mamdouh <aly.mamdouh@interactts.com>
2023-07-23 12:51:27 +0300

    add project Listing Strongly Typed Enum Options

---
+++ Listing Strongly Typed Enum Options in C#/ListingStronglyTypedEnumOptions/ListingStronglyTypedEnumOptions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ListingStronglyTypedEnumOptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
