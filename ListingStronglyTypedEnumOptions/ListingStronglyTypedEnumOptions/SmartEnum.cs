2689207dfeca48980034f83513c533c90632a7bd
Aly Mamdouh <aly.mamdouh@interactts.com>
2023-07-23 14:42:23 +0300

    add generic  SmartEnum  and examples

---
+++ ListingStronglyTypedEnumOptions/ListingStronglyTypedEnumOptions/SmartEnum.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace ListingStronglyTypedEnumOptions
+{
+    public abstract class SmartEnum<TEnum, TValue> where TEnum : SmartEnum<TEnum, TValue>
+    {
+        private static readonly List<TEnum> _list = new List<TEnum>();
+
+        // Despite analysis tool warnings, we want this static bool to be on this generic type (so that each TEnum has its own bool).
+        private static bool _invoked; 
+        public static List<TEnum> List
+        {
+            get
+            {
+                if (!_invoked)
+                {
+                    _invoked = true;
+                    // Force invocaiton/initialization by calling one of the derived members.
+                    typeof(TEnum).GetProperties(BindingFlags.Public | BindingFlags.Static).FirstOrDefault(p => p.PropertyType == typeof(TEnum))?.GetValue(null, null);
+                } 
+                return _list;
+            }
+        }
+
+        public string Name { get; }
+        public TValue Value { get; }
+
+        protected SmartEnum(string name, TValue value)
+        {
+            Name = name;
+            Value = value;
+
+            TEnum item = this as TEnum;
+            List.Add(item);
+        }
+
+        public static TEnum FromName(string name)
+        {
+            return List.Single(item => string.Equals(item.Name, name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public static TEnum FromValue(TValue value)
+        {
+            // Can't use == to compare generics unless we constrain TValue to "class", which we don't want because then we couldn't use int.
+            return List.Single(item => EqualityComparer<TValue>.Default.Equals(item.Value, value));
+        } 
+        public override string ToString() => $"{Name} ({Value})";
+    }
+}
---
+++ ListingStronglyTypedEnumOptions/ListingStronglyTypedEnumOptions/SmartFoo.cs
@@ -0,0 +1,33 @@
+﻿namespace ListingStronglyTypedEnumOptions
+{
+    public class SmartFoo : SmartEnum<SmartFoo, int>
+    {
+        public static SmartFoo A { get; } = new SmartFoo("Foo A", 1);
+        public static SmartFoo B { get; } = new SmartFoo("Foo B", 2);
+
+        private SmartFoo(string name, int value)
+            : base(name, value)
+        { }
+    }
+
+    public class SmartBar : SmartEnum<SmartBar, int>
+    {
+        public static SmartBar A { get; } = new SmartBar("Bar A", 1);
+        public static SmartBar B { get; } = new SmartBar("Bar B", 2);
+        public static SmartBar C { get; } = new SmartBar("Bar C", 3);
+
+        private SmartBar(string name, int value)
+            : base(name, value)
+        { }
+    }
+
+    public class SmartBaz : SmartEnum<SmartBaz, string>
+    {
+        public static SmartBaz A { get; } = new SmartBaz("Baz A", "Baz Val 1");
+        public static SmartBaz B { get; } = new SmartBaz("Baz B", "Baz Val 2");
+
+        private SmartBaz(string name, string value)
+            : base(name, value)
+        { }
+    }
+}
