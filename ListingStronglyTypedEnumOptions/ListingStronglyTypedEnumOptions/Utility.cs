2414fb3f65a5417b7e97719b9740a193ef037d8d
Aly Mamdouh <aly.mamdouh@interactts.com>
2023-07-23 14:43:25 +0300

    using Reflection to get name and display name for enums

---
+++ ListingStronglyTypedEnumOptions/ListingStronglyTypedEnumOptions/Utility.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ListingStronglyTypedEnumOptions
+{
+    public static class Utility
+    {
+        public static string GetEnumName<T>(T value)
+        {
+            return Enum.GetName(typeof(T), value);
+        }
+        public static string GetMultipleEnumsName<T>(string values)
+        {
+            if (string.IsNullOrEmpty(values)) return string.Empty;
+            string newValuesName = null;
+            string[] valuesArray = values.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            foreach (string value in valuesArray)
+            {
+                if (string.IsNullOrEmpty(newValuesName))
+                    newValuesName = $"{Enum.Parse(typeof(T), value)}";
+                else newValuesName += $",{Enum.Parse(typeof(T), value)}";
+            }
+            return newValuesName;
+
+        }
+        public static string? GetEnumDisplayName<T>(T value)
+        {
+            return value?
+                      .GetType()?
+                      .GetMember(value.ToString())
+                      .First()
+                      .GetCustomAttribute<DisplayAttribute>()
+                      ?.GetName();
+        }
+        public static string GetMultipleEnumsDisplayName<T>(string values)
+        {
+            if (string.IsNullOrEmpty(values)) return string.Empty;
+            var enumValues = values.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                              .Select(v => Enum.Parse(typeof(T), v.Trim()))
+                              .Cast<Enum>()
+                              .ToList(); 
+            var displayNames = enumValues.Select(enumValue => GetEnumDisplayName(enumValue)); 
+            return string.Join(", ", displayNames); 
+        }
+    }
+}
